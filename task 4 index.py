e6f046e29d4abf851df53c6992d55c7c98ed170f
rihanarinsi <95712080+rihanarinsi@users.noreply.github.com>
2021-12-15 17:34:45 +0530

    Add files via upload

---
+++ task 4 index.py
@@ -0,0 +1,23 @@
+#iTask 4
+# indexing (reverse & foreward indexing)
+
+
+a = "computer_science"
+
+print(a[-1]) 
+print(a[0]) 
+
+print(a[4])
+print(a[-4])
+
+print(a[2])
+print(a[-2])
+
+print(a[-3])
+print(a[-5]) 
+
+print(a[7])
+print(a[-7])
+
+print(a[8])
+print(a[-8])
